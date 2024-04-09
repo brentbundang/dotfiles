c8927e910b179b30906371e2894137565b4733bf
Brent Bundang <brent.bundang@gmail.com>
2024-04-09 07:11:24 -0400

    adding nvim configs

---
+++ lua/core/mappings.lua
@@ -0,0 +1,25 @@
+local map = vim.keymap.set 
+vim.g.mapleader = " "
+map("n", "-", vim.cmd.Ex)
+
+
+-- nvimtree
+map("n", "<C-n>", "<cmd>NvimTreeToggle<CR>", { desc = "Nvimtree Toggle window" })
+map("n", "<leader>e", "<cmd>NvimTreeFocus<CR>", { desc = "Nvimtree Focus window" })
+
+-- window navigation
+map("n", "<C-h>", "<C-w>h", { desc = "Switch Window left" })
+map("n", "<C-l>", "<C-w>l", { desc = "Switch Window right" })
+map("n", "<C-j>", "<C-w>j", { desc = "Switch Window down" })
+map("n", "<C-k>", "<C-w>k", { desc = "Switch Window up" })
+
+map("n", "<C-s>", "<cmd>w<CR>", { desc = "File Save" })
+
+map("n", "<leader>n", "<cmd>set nu!<CR>", { desc = "Toggle Line number" })
+
+--telescope
+map("n", "<leader>fo", "<cmd>Telescope oldfiles<CR>", { desc = "Telescope Find oldfiles" })
+map("n", "<leader>fz", "<cmd>Telescope current_buffer_fuzzy_find<CR>", { desc = "Telescope Find in current buffer" })
+map("n", "<leader>cm", "<cmd>Telescope git_commits<CR>", { desc = "Telescope Git commits" })
+map("n", "<leader>gt", "<cmd>Telescope git_status<CR>", { desc = "Telescope Git status" })
+map("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Telescope Find files" })
---
+++ lua/core/plugins.lua
@@ -0,0 +1,73 @@
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
+return require('packer').startup(function(use)
+	use 'wbthomason/packer.nvim'
+
+	use {
+		'nvim-telescope/telescope.nvim', tag = '0.1.6',
+		-- or                            , branch = '0.1.x',
+		requires = { {'nvim-lua/plenary.nvim'} }
+	}
+	use "nvim-lua/plenary.nvim"
+	use {
+		'nvim-treesitter/nvim-treesitter',
+		run = ':TSUpdate'
+	}
+	use "rebelot/kanagawa.nvim"
+	use { "jiaoshijie/undotree" }
+	use {
+		'nvim-tree/nvim-tree.lua',
+		requires = {
+			'nvim-tree/nvim-web-devicons', -- optional
+		},
+	}
+	use {
+		"williamboman/mason.nvim",
+		"williamboman/mason-lspconfig.nvim",
+		"neovim/nvim-lspconfig",
+
+		-- Autocompletion
+		{'hrsh7th/nvim-cmp'},
+		{'hrsh7th/cmp-buffer'},
+		{'hrsh7th/cmp-path'},
+		{'saadparwaiz1/cmp_luasnip'},
+		{'hrsh7th/cmp-nvim-lsp'},
+		{'hrsh7th/cmp-nvim-lua'},
+
+		-- Snippets
+		{'L3MON4D3/LuaSnip'},
+		{'rafamadriz/friendly-snippets'},
+	}
+	use {
+		'VonHeikemen/lsp-zero.nvim',
+		branch = 'v3.x',
+		requires = {
+			{'neovim/nvim-lspconfig'},
+		}
+	}
+	use({
+		"aserowy/tmux.nvim",
+		config = function() return require("tmux").setup() end
+	})
+
+
+
+   
+
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if packer_bootstrap then
+    require('packer').sync()
+  end
+end)
